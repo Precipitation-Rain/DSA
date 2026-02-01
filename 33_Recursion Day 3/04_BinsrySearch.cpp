241f04b5045bf6cb07cc530640dd5ac8d5f223f9
Precipitation-Rain <rajvardhanshewale1200@gmail.com>
2026-02-01 23:25:20 +0530

    Added lec 33 reamining files

---
+++ 33_Recursion Day 3/04_BinsrySearch.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+bool binarySearch(int * arr , int start , int end , int element)
+    {
+        
+
+        // base case
+        if( start > end)
+            return false;
+
+        //preprocessing
+        int mid = start + (end - start) / 2;
+
+        if(arr[mid] == element)
+            {
+                return true;
+            }
+        
+        else if( arr[mid] < element)
+                {
+                    binarySearch( arr , mid + 1 , end , element);
+                }
+
+        else    
+                {
+                    binarySearch( arr , start , mid - 1 , element);
+                }
+
+    }
+
+int main()
+    {
+        int arr[] = {2,3,4,13,17,49};
+        int size = 6;
+        int element;
+        
+        cout <<"Enter element to seacrh : ";
+        cin >> element;
+
+        if(binarySearch(arr , 0 , size , element))
+            {
+                cout <<"Element is present " << endl;
+            }
+        else
+            {
+                cout <<"Element is not present " << endl;
+            }
+
+    }
